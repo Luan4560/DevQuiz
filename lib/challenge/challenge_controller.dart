a87e57234d824594668efbaac55dd0c07262eec2
Luan Nascimento <luan.nascimento11@outlook.com>
2021-04-22 18:05:13 -0300

    Navigation and awnsers

---
+++ lib/challenge/challenge_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class ChallengeController {
+  final currentPageNotifier = ValueNotifier<int>(0);
+  int get currentPage => currentPageNotifier.value;
+  set currentPage(int value) => currentPageNotifier.value = value;
+}
